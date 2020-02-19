f5ede149a9529ff1142a57a646ed51678ef7b048
Gene Chen <gene@sonar.software>
2020-02-19 01:01:03 -0800

    Initial implementation

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd "$GITHUB_WORKSPACE"
+
+export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
+
+/usr/local/bin/phpcs.phar \
+    --report=summary \
+    --report-checkstyle=/tmp/phpcs_result_checkstyle.xml \
+    ${INPUT_PHPCS_ARGS:-\.}
+
+EXIT_CODE=$?
+
+< /tmp/phpcs_result_checkstyle.xml | reviewdog -f=checkstyle -name="phpcs" -reporter="${INPUT_REPORTER:-github-pr-check}"
+
+exit $EXIT_CODE
