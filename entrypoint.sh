ba4f75c8bb456b7c7da371c1265fd408e28c3378
Gene Chen <gene@sonar.software>
2021-11-03 12:06:37 -0700

    Filter to only php extension

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 /usr/local/bin/phpcs.phar \
     --report=summary \
     --report-checkstyle=/tmp/phpcs_result_checkstyle.xml \
+    --extensions=php \
     ${INPUT_PHPCS_ARGS:-\.}
 
 EXIT_CODE=$?
